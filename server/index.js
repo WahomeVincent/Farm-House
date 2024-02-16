e81fec0b327fd11e54ba7e7ec17017c321e8fe50
Wahome <vinwahome@gmail.com>
2024-02-16 12:57:05 +0300

    Connect App to the database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,48 @@
 const express = require('express');
-const app = express()
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
+// Express app
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000
+
+
+// Db connection
+console.log(process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URL)
+    .then(console.log("Database is connected"))
+    .catch(err => console.log(err))
+
+
+// Schema
+const newproductSchema = mongoose.Schema({
+    name : String,
+    category : String,
+    price : Number,
+    image: String,
+    description: String,
+})
+
+//Model
+
+const newproductModel = mongoose.model('newproduct', newproductSchema)
+
+
+app.get('/', (req, res) => {
+    res.send("Server is running")
+})
+
+app.post('/newproduct', (req, res) => {
+    const data = newproductModel(req.body)
+    const dataSave = data.save()
+    res.send({message : 'Product added to db successfully.'})
+})
 
 
 app.listen(8000, (req, res) => {
-    console.log('App is running');
+    console.log('Server is running on port : ' + PORT);
 })
