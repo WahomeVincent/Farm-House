8d44910158926dcd2e45cb49ff73460986a3416a
Wahome <vinwahome@gmail.com>
2024-02-25 14:29:39 +0300

    Add a spinner when data is loading

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -18,32 +18,57 @@ function Categories() {
   });
 
   const uniqueCategories = [...new Set(productData.map((item) => item.category))];
+  const loadingArray = new Array(3).fill(null)
+  
 
   return (
     <div className=''>
       <h1 className='mx-4 mt-2 underline text-xl font-bold'>Our Products</h1>
       <div className='container md:justify-evenly' {...bind()}>
-        {uniqueCategories.map((category) => {
-          // Specify the URL for each category
-          const categoryUrl = `/products/${category.toLowerCase()}`;
+        {productData[0] ?
+           
+              uniqueCategories.map((category) => {
+                // Specify the URL for each category
+                const categoryUrl = `/products/${category.toLowerCase()}`;
 
-          return (
-            <Link to={categoryUrl} key={category} className='flex flex-col items-center'>
-              <animated.div
-                className='card shadow-lg drop-shadow-lg'
-                style={{
-                  ...style,
-                  backgroundImage: `url('https://cdn.pixabay.com/photo/2015/05/04/10/16/vegetables-752153_1280.jpg')`,
-                }}
-              />
+                return (
+                  <Link to={categoryUrl} key={category} className='flex flex-col items-center'>
+                    <animated.div
+                      className='card shadow-lg drop-shadow-lg'
+                      style={{
+                        ...style,
+                        backgroundImage: `url('https://cdn.pixabay.com/photo/2015/05/04/10/16/vegetables-752153_1280.jpg')`,
+                      }}
+                    />
 
-              <h1 className='text-lg font-mono'>{category}</h1>
-            </Link>
-          );
-        })}
+                    <h1 className='text-lg font-mono'>{category}</h1>
+                  </Link>
+                );
+              })
+              
+        : loadingArray.map((item, index) => {
+                  return(
+                    <div key={index}>
+                         <animated.div
+                              className='card loader shadow-lg drop-shadow-lg'
+                              style={{
+                                ...style,
+                                // backgroundImage: `url('https://cdn.pixabay.com/photo/2015/05/04/10/16/vegetables-752153_1280.jpg')`,
+                              }}
+                              
+                            />
+
+                            <h1 className='text-lg mx-4 font-mono'>Loading...</h1>
+                    </div>
+                  )
+                })
+        }
+            
+        
       </div>
     </div>
   );
 }
 
 export default Categories;
+
